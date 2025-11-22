be3134a1bed0fff6bc8f87ed23a447f276cf36bf
tajinderjsingh-maker <tajinderdigitaldesign@gmail.com>
2025-11-22 20:17:53 +0530

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// REQUIRED for GitHub Pages
-export default defineConfig({
-  base: "/genai-apps/",
-  plugins: [react()]
-});
