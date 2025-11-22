d25d9368940392ae8d02f75385cb56be72eca2d9
tajinderjsingh-maker <tajinderdigitaldesign@gmail.com>
2025-11-22 19:43:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// REQUIRED for GitHub Pages
 export default defineConfig({
   base: "/genai-apps/",
-  plugins: [react()],
+  plugins: [react()]
 });
